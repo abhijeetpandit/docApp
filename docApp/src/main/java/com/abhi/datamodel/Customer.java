76589f4976321e8fd494a06b0fe096586d3c91be
abpandit <abpandit@DIN52002239.corp.capgemini.com>
2014-07-08 18:54:35 +0530

    First commit

---
+++ docApp/src/main/java/com/abhi/datamodel/Customer.java
@@ -0,0 +1,5 @@
+package com.abhi.datamodel;
+
+public class Customer {
+	
+}
---
+++ docApp/src/main/java/com/abhi/datamodel/User.java
@@ -0,0 +1,47 @@
+package com.abhi.datamodel;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName
+				+ ", firstName=" + firstName + ", middleName=" + middleName
+				+ ", lastName=" + lastName + "]";
+	}
+}
